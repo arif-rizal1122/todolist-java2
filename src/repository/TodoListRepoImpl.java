b6d98db7ff64a748b0bdc0a3ab4e01e5d3be002d
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-04 08:07:58 +0800

    get All () impl

---
+++ src/repository/TodoListRepoImpl.java
@@ -0,0 +1,23 @@
+package repository;
+
+import entity.TodoList;
+
+public class TodoListRepoImpl implements TodoListRepo{
+    private TodoList[] data = new TodoList[10];
+
+
+    @Override
+    public TodoList[] getAll() {
+        return data;
+    }
+
+    @Override
+    public void add(TodoList todoList) {
+
+    }
+
+    @Override
+    public void remove(Integer number) {
+
+    }
+}
---
+++ src/service/TodoListServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import entity.TodoList;
+import repository.TodoListRepo;
+
+public class TodoListServiceImpl implements TodolistService {
+    private TodoListRepo todoListRepo;
+
+    public TodoListServiceImpl(TodoListRepo todoListRepo) {
+        this.todoListRepo = todoListRepo;
+    }
+
+    @Override
+    public void showTodoList() {
+        System.out.println("TODOLIST");
+        TodoList[] model = todoListRepo.getAll();
+
+        for (int i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var nomor =  i + 1;
+            if (todo != null){
+                System.out.println(nomor + ". " + todo);
+            }
+        }
+    }
+
+
+    @Override
+    public void addTodoList(String todo) {
+
+    }
+
+    @Override
+    public void removeTodoList(Integer number) {
+
+    }
+
+}
